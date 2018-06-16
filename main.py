623bda2d6582d323d48d176e2583693823b2649f
Peter Hartmann <github@peterh.33mail.com>
2018-06-16 21:35:43 +0200

    play with parameters a bit
    Accuracy: 64.6 %

--- main.py
+++ main.py
@@ -146,8 +146,8 @@ def cnn_model_fn(features, labels, mode):
 
     # Configure the Training Op (for TRAIN mode)
     if mode == tf.estimator.ModeKeys.TRAIN:
-        # optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001)
-        optimizer = tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.0001)
+        #optimizer = tf.train.MomentumOptimizer(learning_rate=0.001, momentum=0.9)
         train_op = optimizer.minimize(
             loss=loss,
             global_step=tf.train.get_global_step())
